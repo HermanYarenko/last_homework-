a28357376a034b88906f993a864c93602ec79e81
HermanYarenko <teyonel@icloud.com>
2022-12-30 14:57:29 +0300

    ok

---
+++ DZ_zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿int m = InputInt("Введите первое число:");
+int n = InputInt("Введите второе число:");
+Console.WriteLine($"Сумма промежуточных чисел от {m} до {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
